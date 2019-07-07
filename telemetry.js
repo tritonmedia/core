cd42225f730716d3236b07b2e71ead576253d281
Jared Allard <jaredallard@outlook.com>
2019-07-06 21:36:00 -0700

    feat: telemetry emitProgress

--- telemetry.js
+++ telemetry.js
@@ -11,6 +11,10 @@
 const AMQP = require('./amqp')
 const proto = require('./proto')
 const uuid = require('uuid/v4')
+const path = require('path')
+const logger = require('pino')({
+  name: path.basename(__filename)
+})
 
 const required = () => {
   throw new Error('Missing required argument.')
@@ -18,16 +22,20 @@ const required = () => {
 
 module.exports = class Telemetry {
   constructor (amqpHost) {
-    this.publisher = new AMQP(amqpHost)
+    this.statusPublisher = new AMQP(amqpHost)
+    this.progressPublisher = new AMQP(amqpHost)
   }
 
   async connect () {
     this.telemetryStatusProto = await proto.load('api.TelemetryStatus')
-    await this.publisher.connect()
+    this.telemetryProgressProto = await proto.load('api.TelemetryProgress')
+
+    await this.statusPublisher.connect()
+    await this.progressPublisher.connect()
   }
 
   /**
-   * Emit a Status Update
+   * Emit a status update
    *
    * @param {String} mediaId id of the media
    * @param {Number} status status number
@@ -48,9 +56,40 @@ module.exports = class Telemetry {
     }
 
     try {
-      await this.publisher.publish('v1.telemetry.status', encoded)
+      await this.statusPublisher.publish('v1.telemetry.status', encoded)
     } catch (err) {
       logger.warn('failed to publish telemetry status:', err.message || err)
+    }
+
+    return payload
+  }
+
+  /**
+   * Emit a progress update
+   *
+   * @param {String} mediaId id of the media
+   * @param {Number} status status number
+   * @param {Number} progress 0 - 100 progress status
+   */
+  async emitProgress (mediaId = required(), status = required(), progress = required()) {
+    const payload = {
+      id: uuid(),
+      mediaId,
+      status,
+      progress
+    }
+
+    let encoded;
+    try {
+      encoded = proto.encode(this.telemetryProgressProto, payload)
+    } catch (err) {
+      logger.warn('Failed to serialize telemetry progress update:', err.message || err)
+    }
+
+    try {
+      await this.progressPublisher.publish('v1.telemetry.progress', encoded)
+    } catch (err) {
+      logger.warn('failed to publish telemetry progress:', err.message || err)
     }
 
     return payload
